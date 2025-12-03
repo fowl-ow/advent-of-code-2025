49d3cbf2ec06516787b4dd1c01f23b4789493baa
fowl-ow <80642246+fowl-ow@users.noreply.github.com>
2025-12-03 22:08:29 +0100

    feat: implement day 2 part 2

--- day-2/day-2.hs
+++ day-2/day-2.hs
@@ -2,16 +2,6 @@
 import Data.List.Split (splitOn)
 import Numeric (readInt)
 
-----------------------------
----------- Part 1 ----------
-----------------------------
-
-main :: IO ()
-main = do
-    contents <- readFile "input.txt"
-    print $ show $ catAndSum $ map invalidNums $ extractRanges contents
-
--- evens n = [x | x <- [1 .. n], even x]
 extractTextRanges :: String -> [String]
 extractTextRanges = splitOn ","
 
@@ -23,6 +13,18 @@ extractRanges = map toPair . extractTextRanges
             [a, b] -> (read a, read b)
             _ -> error "invalid range"
 
+catAndSum :: [[Int]] -> Int
+catAndSum = sum . concat
+
+----------------------------
+---------- Part 1 ----------
+----------------------------
+
+-- main :: IO ()
+-- main = do
+--     contents <- readFile "input.txt"
+--     print $ catAndSum $ map invalidNums $ extractRanges contents
+
 invalidNums :: (Int, Int) -> [Int]
 invalidNums (from, to) = [x | x <- [from .. to], invalid x]
 
@@ -32,14 +34,31 @@ invalid x = front == back
     s = show x
     (front, back) = splitAt (length s `div` 2) s
 
-catAndSum :: [[Int]] -> Int
-catAndSum = sum . concat
-
 ----------------------------
 ---------- Part 2 ----------
 ----------------------------
 
--- main :: IO ()
--- main = do
---     contents <- readFile "input.txt"
---     print $ show $ catAndSum $ map invalidNums $ extractRanges contents
+main :: IO ()
+main = do
+    contents <- readFile "input.txt"
+    print $ catAndSum $ map generalInvalidNums $ extractRanges contents
+
+generalInvalidNums :: (Int, Int) -> [Int]
+generalInvalidNums (from, to) = [x | x <- [from .. to], generalInvalid x]
+
+generalInvalid :: Int -> Bool
+generalInvalid x = foldr ((||) . allEqual) False lol
+  where
+    s = show x
+    lol = map (`chunksOf` s) $ sublistSizes $ length s
+
+sublistSizes :: Int -> [Int]
+sublistSizes n = [x | x <- [1 .. n `div` 2], n `mod` x == 0]
+
+chunksOf _ [] = []
+chunksOf n xs = take n xs : chunksOf n (drop n xs)
+
+allEqual :: [String] -> Bool
+allEqual [] = True
+allEqual [_] = True
+allEqual (a : as) = fst $ foldr (\x (okSoFar, ref) -> (okSoFar && x == ref, ref)) (True, a) as
